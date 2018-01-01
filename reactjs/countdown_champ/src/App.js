cc59d73522d5e977707af22872d6c0d07129302b
Saumay Agrawal <saumay.agrawal@gmail.com>
2018-01-02 00:33:39 +0530

    Created the countdown template

---
+++ reactjs/countdown_champ/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            deadline: 'December 25, 2018',
+            newDeadline: ''
+        }
+    }
+
+    changeDeadline() {
+        this.setState({ deadline: this.state.newDeadline});
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <div className='App-title'>Countdown to {this.state.deadline}</div>
+                <div>
+                    <div className='Clock-days'>14 days</div>
+                    <div className='Clock-hours'>15 hours</div>
+                    <div className='Clock-minutes'>20 minutes</div>
+                    <div className='Clock-seconds'>30 seconds</div>
+                </div>
+                <div>
+                    <input
+                        placeholder='new data'
+                        onChange={event => this.setState({ newDeadline: event.target.value})}
+                    />
+                    <button onClick={() => this.changeDeadline()}>
+                        Submit
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
